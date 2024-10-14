32ad1d62730ed846cbd973089a4f668d375a7b09
Farhan-nur <anun2804@gmail.com>
2024-10-14 19:42:00 +0700

    Add files via upload

---
+++ Week 8/Tugas 4.py
@@ -0,0 +1,5 @@
+student_info = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+student_info['Age'] = 8
+student_info['School'] = "DPS School"
+print("student_info['Age']: ", student_info['Age'])
+print("student_info['School']: ", student_info['School'])
