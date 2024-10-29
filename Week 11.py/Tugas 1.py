b3809215a480932bce42626f7b6bdbd69d9f99c2
Farhan-nur <anun2804@gmail.com>
2024-10-29 09:29:35 +0700

    Add files via upload

---
+++ Week 11.py/Tugas 1.py
@@ -0,0 +1,7 @@
+# Definisi fungsi print_string
+def print_string(str):
+    """Menampilkan argumen string str ke layar"""
+    print(str)
+
+# Memanggil fungsi dengan kata kunci
+print_string(str="Hello Python")
---
+++ Week 11.py/Tugas 2.py
@@ -0,0 +1,8 @@
+# Definisi fungsi
+def print_info(nama, usia):
+    """Fungsi ini menampilkan informasi yang diberikan."""
+    print("Nama:", nama)
+    print("Usia:", usia)
+
+# Memanggil fungsi dengan argumen yang sesuai
+print_info(nama="Budi", usia=25)
